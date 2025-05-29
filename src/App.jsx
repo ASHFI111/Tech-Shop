6a4b8551d0bea5962de2def60d513b92a2882d40
ASHFI PROJECT <145926881+ASHFI111@users.noreply.github.com>
2025-05-29 09:54:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(true);
-    }, 2000); // 2 seconds
+    }, 2000); // 5 seconds
 
     return () => clearTimeout(timer);
   }, []);
